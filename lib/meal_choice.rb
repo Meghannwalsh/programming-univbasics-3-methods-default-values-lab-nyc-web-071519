39af43d3476fc48ca01d33555c62d1653802ed92
Meghann McCool Walsh <meghann.mccool.walsh@gmail.com>
2019-07-03 14:20:43 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,5 +1,5 @@
 # Your code here
-def meal_choice(veg, side, protien="meat")
+def meal_choice(veg1, veg2, protien="meat")
   puts "A plate of #{protien} with #{veg} and #{side}."
   puts "What a nutritious meal!"
 end
